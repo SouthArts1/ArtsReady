8323f537ec935dbcc157c825d5e1a24e13b7c6a4
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-05-31 09:54:28 -0400

    rails 3.0.7 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artsready::Application.config.secret_token = '1ab9a9c982cd998fdf3c276e64010ec5ec23e82ab2800e50a9e2e9986b41f32661721ac9cda1b82e05c6361ccfc4b9b6584e9f7f463e4b85ed88482511d429c5'
