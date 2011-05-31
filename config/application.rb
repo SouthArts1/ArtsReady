cfa4ddc8a0616e7931ab4e16643f24068fe38e45
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-05-31 10:11:08 -0400

    added rspec and configured to use factory girl while skipping view and helper specs

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module Artsready
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => true, :views => false, :helpers => false 
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
