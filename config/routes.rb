35c5c516a40376bbda8fcb3c44d1a8cddb23feab
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-01 18:21:37 -0400

    tweaked routes and updated wireframe content to reflect real paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Artsready::Application.routes.draw do
 
   get "sign_up" => "users#new", :as => "sign_up"  
   get "sign_in" => "sessions#new", :as => "sign_in"  
+  post "sign_in" => "sessions#create"
   get "sign_out" => "sessions#destroy", :as => "sign_out"  
-  resources :users, :sessions
+  
+  resources :users
   get "member/index"
   get "home/index"
   root :to => "home#index"
