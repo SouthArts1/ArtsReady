d3fc9108be7d80d948bcbe21f7cb11314419203d
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-01 14:34:36 -0400

    added bcrypt-encrypted password

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.7'
 gem 'mysql2', '0.2.6'
 
+gem 'bcrypt-ruby', :require => 'bcrypt' 
+
 # deploy
 gem 'rake', '0.8.7'
 gem 'newrelic_rpm'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
   
   attr_accessor :password 
+  
+  before_save :encrypt_password
+  
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
+
   validates_confirmation_of :password, :on => :create
   validates_uniqueness_of :email
+  
+  def encrypt_password  
+    if password.present?  
+      self.encrypted_password = BCrypt::Password.create(password)  
+    end  
+  end
+
 end
