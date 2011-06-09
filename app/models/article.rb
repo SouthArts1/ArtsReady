85b00e9c18dee08668995c2ce5f8faa6d85dac49
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-09 10:10:51 -0400

    converted article hashes to AR

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,15 +6,14 @@ class Article < ActiveRecord::Base
   validates_presence_of :content
 
   def self.featured
-    [{"id" => 1, "title" => 'Featured Library Article', "published_on" => 1.day.ago.to_date, "content" => 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean eu leo quam. Sed posuere at lobortis. Aenean lacinia bibendum nulla sed consectetur.\nThis is meant to be a leader to draw people in.'}]
+    [Article.first]
   end
 
   def self.recent
-    [
-      {"id" => 2, "title" => 'Library Article Title', "published_on" => 1.week.ago.to_date},
-      {"id" => 3, "title" => 'Another Library Article Title', "published_on" => 1.month.ago.to_date},
-      {"id" => 4, "title" => 'Yet Another Library Article Title', "published_on" => 1.year.ago.to_date}
-    ]
+    Article.all - self.featured
   end
   
+  def published_on
+    created_at.to_date
+  end
 end
