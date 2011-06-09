a19fa5412384e9ba0dc61b9078abe6ed533eb68f
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-09 09:15:27 -0400

    requiring title and content, displaying

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
-  attr_accessible :name, :context
+  
+  attr_accessible :title, :content
+  
+  validates_presence_of :title
+  validates_presence_of :content
 
   def self.featured
     [{"id" => 1, "title" => 'Featured Library Article', "published_on" => 1.day.ago.to_date, "content" => 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean eu leo quam. Sed posuere at lobortis. Aenean lacinia bibendum nulla sed consectetur.\nThis is meant to be a leader to draw people in.'}]
@@ -11,10 +15,6 @@ class Article < ActiveRecord::Base
       {"id" => 3, "title" => 'Another Library Article Title', "published_on" => 1.month.ago.to_date},
       {"id" => 4, "title" => 'Yet Another Library Article Title', "published_on" => 1.year.ago.to_date}
     ]
-  end
-  
-  def title
-    name
   end
   
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Article do
-  it "should be valid" do
-    Article.new.should be_valid
-  end
+  subject { Article.new }
+  
+  it { should validate_presence_of(:title)} 
+  it { should validate_presence_of(:content)}
+
 end
