50176787501103ed58736f6d9f712ea27162fc77
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-09 22:10:10 -0400

    added views for list and detail of public library items

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Artsready::Application.routes.draw do
   get "sitemap" => "home#sitemap", :as => "sitemap"
   get "support" => "home#support", :as => "support"
   get "tour" => "home#tour", :as => "tour"
+
+  get "library" => "home#library", :as => "library"
+  get "public_article" => "home#public_article", :as => "public_article"
   
   get "tbd" => "home#tbd", :as => "tbd"
   root :to => "home#index"
