26264d7a1cb0503e83fa86c7b6bc2ffed5716cb7
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-12 09:17:45 -0400

    used comp to layout assessment titles

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -5,10 +5,10 @@ class Assessment < ActiveRecord::Base
   
   CRITICAL_FUNCTIONS = [
     {:name => 'people', :title => 'People Resources', :optional => false},
-    {:name => 'finance', :title => 'Finances &amp; Insurance', :optional => false},
+    {:name => 'finance', :title => 'Finances & Insurance', :optional => false},
     {:name => 'tech', :title => 'Technology', :optional => false},
     {:name => 'performances', :title => 'Productions', :optional => 'We put on performances'},
-    {:name => 'tickets', :title => 'Tickets &amp; Messaging', :optional => 'We manage ticket sales'},
+    {:name => 'tickets', :title => 'Tickets & Messaging', :optional => 'We manage ticket sales'},
     {:name => 'facilities', :title => 'Facilities', :optional => 'We have our own space/facility'},
     {:name => 'programs', :title => 'Programs', :optional => 'We manage ongoing public programs'},
     {:name => 'grants', :title => 'Grantmaking', :optional => 'We provide grants'},
