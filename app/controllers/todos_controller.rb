a2ea1c704959fb1cc6eabb05d9d26d326e1f8b47
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-20 15:26:54 -0400

    basic add/edit for todo with reference to user?

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,29 @@ class TodosController < ApplicationController
   
   def index
     @todos = current_org.todos
+    @todo = current_org.todos.new
+  end
+  
+  def show
+    @todo = current_org.todos.find(params[:id])
+  end
+
+  def edit
+    @todo = current_org.todos.find(params[:id])
+  end
+  
+  def create
+    @todo = current_org.todos.new(params[:todo])
+
+    if @todo.save
+      redirect_to todos_path, :notice => 'Post was successfully created.'
+    else
+      render :action => "index"
+    end
+
+  end
+  
+  def update
   end
   
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,9 @@ class Todo < ActiveRecord::Base
   belongs_to :organization
   belongs_to :answer
   belongs_to :action_item
+  belongs_to :user
   
-  delegate :description, :to => :action_item, :allow_nil => false, :prefix => false
+  delegate :description, :to => :action_item, :allow_nil => true, :prefix => false
+  delegate :recurrence, :to => :action_item, :allow_nil => true, :prefix => false
+  delegate :name, :to => :user, :allow_nil => true, :prefix => true
 end
