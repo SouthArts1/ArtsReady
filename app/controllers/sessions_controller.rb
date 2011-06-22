849c83c94d396709ada3a958d5e73a4370439d18
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-06-22 14:19:56 -0400

    prettied up the flash using space from the weather alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id  
       redirect_to dashboard_path, :notice => "Logged in!"  
     else  
-      flash.now.alert = "Invalid email or password"  
+      flash[:warning] = "Invalid email or password"  
       render "new"  
     end  
   end
