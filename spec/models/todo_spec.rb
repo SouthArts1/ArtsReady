ad7ae1375457e88bee49b3968e8738a0597ac8de
Kevin Hopkins <kevin@wearefound.com>
2011-07-05 11:56:11 -0400

    added spec test for todo to accept a change in user.  tested through UI

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,9 +4,17 @@ describe Todo do
   it { should belong_to(:answer) }
   it { should belong_to(:action_item) }
   it { should belong_to(:organization) }
+  it { should belong_to(:user) }
   
   it "should accept a due date" do
     todo = Factory.build(:todo)
     todo.due_on.should_not be_nil
   end
+  
+  it "should allow its user to be changed" do
+    todo = Factory.build(:todo)
+    todo.user_id = 2
+    todo.save.should be_true
+  end
+  
 end
