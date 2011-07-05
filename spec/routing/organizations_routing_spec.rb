6dd11003d5e03710bf6673ae22204a7fe206601d
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-07-05 16:30:37 -0400

    routing spec for organizations

--- spec/routing/organizations_routing_spec.rb
+++ spec/routing/organizations_routing_spec.rb
@@ -3,14 +3,6 @@ require "spec_helper"
 describe OrganizationsController do
   describe "routing" do
 
-    it "routes to #index" do
-      get("/organizations").should route_to("organizations#index")
-    end
-
-    it "routes to #new" do
-      get("/organizations/new").should route_to("organizations#new")
-    end
-
     it "routes to #show" do
       get("/organizations/1").should route_to("organizations#show", :id => "1")
     end
@@ -19,16 +11,23 @@ describe OrganizationsController do
       get("/organizations/1/edit").should route_to("organizations#edit", :id => "1")
     end
 
-    it "routes to #create" do
-      post("/organizations").should route_to("organizations#create")
-    end
-
     it "routes to #update" do
       put("/organizations/1").should route_to("organizations#update", :id => "1")
     end
 
+    it "routes to #index" do
+      get("/organizations").should_not be_routable
+    end
+
+    it "routes to #new" do
+      get("/organizations/new").should_not be_routable
+    end
+    it "routes to #create" do
+      post("/organizations").should_not be_routable
+    end
+
     it "routes to #destroy" do
-      delete("/organizations/1").should route_to("organizations#destroy", :id => "1")
+      delete("/organizations/1").should_not be_routable
     end
 
   end
