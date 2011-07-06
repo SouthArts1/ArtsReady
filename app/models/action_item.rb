85ef131b55aea7d15f69c5067017693a757e4587
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-07-06 15:34:51 -0400

    validity smoke tests

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -1,6 +1,8 @@
 class ActionItem < ActiveRecord::Base
   belongs_to :question
   has_many :todos
-  
+
   validates_presence_of :question
+  validates_presence_of :description
+  
 end
--- spec/models/action_item_spec.rb
+++ spec/models/action_item_spec.rb
@@ -5,5 +5,19 @@ describe ActionItem do
   it { should have_many(:todos)}
   
   it { should validate_presence_of(:question)}
+  it { should validate_presence_of(:description)}
   it { should_not be_valid }
+  
+  context "#create" do
+    it "should be valid with required params" do
+      ActionItem.create(:description => 'New Action Item', :question => Factory(:question)).should be_valid
+    end
+    it "should fail without required params" do
+      ActionItem.create(:description => 'New Action Item').should_not be_valid
+    end
+    it "should fail without required params" do
+      ActionItem.create(:question => Factory(:question)).should_not be_valid
+    end
+    
+  end
 end
