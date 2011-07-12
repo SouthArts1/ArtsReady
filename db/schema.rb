0ddfc6619ba6c9af023bf5d7a7ff5a489585bc2c
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-07-12 06:39:36 -0400

    postgresql tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110707181213) do
+ActiveRecord::Schema.define(:version => 20110712101452) do
 
   create_table "action_items", :force => true do |t|
     t.string   "description"
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(:version => 20110707181213) do
   end
 
   create_table "questions", :force => true do |t|
-    t.string   "description"
+    t.text     "description"
     t.string   "critical_function"
     t.integer  "import_id"
     t.datetime "created_at"
