34af8725be6c85ecf25d0d728e69ad8e2365c813
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-07-15 05:59:06 -0400

    default in schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(:version => 20110714181246) do
     t.integer  "question_id"
     t.string   "preparedness"
     t.string   "priority"
-    t.boolean  "was_skipped"
+    t.boolean  "was_skipped",   :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
