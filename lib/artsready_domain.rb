94bd7f0761b1ab0f8815effd8e5585bab7d13e2a
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-07-20 14:58:49 -0400

    updated critical_function assignment in seeds and got list in sync with domain constants

--- lib/artsready_domain.rb
+++ lib/artsready_domain.rb
@@ -3,12 +3,12 @@ module ArtsreadyDomain
   CRITICAL_FUNCTIONS = [
     {:name => 'people', :title => 'People Resources', :optional => false},
     {:name => 'finance', :title => 'Finances & Insurance', :optional => false},
-    {:name => 'tech', :title => 'Technology', :optional => false},
-    {:name => 'performances', :title => 'Productions', :optional => 'We put on performances'},
-    {:name => 'tickets', :title => 'Tickets & Messaging', :optional => 'We manage ticket sales'},
+    {:name => 'technology', :title => 'Technology', :optional => false},
+    {:name => 'productions', :title => 'Productions', :optional => 'We put on performances'},
+    {:name => 'ticketing', :title => 'Tickets & Messaging', :optional => 'We manage ticket sales'},
     {:name => 'facilities', :title => 'Facilities', :optional => 'We have our own space/facility'},
     {:name => 'programs', :title => 'Programs', :optional => 'We manage ongoing public programs'},
-    {:name => 'grants', :title => 'Grantmaking', :optional => 'We provide grants'},
+    {:name => 'grantmaking', :title => 'Grantmaking', :optional => 'We provide grants'},
     {:name => 'exhibits', :title => 'Exhibits', :optional => 'We put on exhibits'}
   ]
 
