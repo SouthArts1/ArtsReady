fdb5374ae0547f9283f09fa1eea18731084be547
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-07-27 16:59:00 -0400

    added email when user is created

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ActionMailer::Base
+  include Rails.application.routes.url_helpers
+  default :from => "no-reply@artsready.org"
+  layout 'email'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome.subject
+  #
+  def welcome(user)
+    @greeting = "Hi"
+    @user = user
+    mail :to => user.email
+  end
+end
---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe UserMailer do
+  describe "welcome" do
+    let(:user) { Factory(:user) }
+    let(:mail) { UserMailer.welcome(user) }
+
+    it "renders the headers" do
+      mail.subject.should eq("Welcome")
+      mail.to.should eq([user.email])
+      mail.from.should eq(["no-reply@artsready.org"])
+    end
+
+    it "renders the body" do
+      mail.body.encoded.should match("Hi")
+    end
+  end
+
+end
