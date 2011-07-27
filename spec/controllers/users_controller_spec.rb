778ba8007682015bebd5a1c619fb088ab873e099
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-07-27 13:24:01 -0400

    removed method so removed test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,18 +2,4 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-    it "should load a user" do
-      get 'new'
-      assigns(:user).should be_present
-    end
-  end
-
-  describe "GET 'create'" do
-  end
-
 end
