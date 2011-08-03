d145140dc00759925883548993cb4d0b153f32bd
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-08-03 14:07:34 -0400

    refactored users to new factory_girl format

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,43 +1,43 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
+FactoryGirl.define do
 
-Factory.define :user do |f|
-  f.first_name "First"
-  f.last_name "Last"
-  f.sequence(:email) {|n| "person#{n}@example.com" }
-  f.password "password"
-  f.password_confirmation {|p| p.password}
-  f.role 'reader'
-  f.admin false
-  f.organization Factory(:organization)
-end
+  factory :user, :aliases => [:reader] do
+    first_name "First"
+    last_name "Last"
+    sequence(:email) {|n| "person#{n}@example.com" }
+    password "password"
+    password_confirmation {|p| p.password}
+    role 'reader'
+    admin false
+    association :organization, :factory => :organization  
 
-Factory.define :reader, :parent => :user do |f|
-end
+    factory :editor do
+      role 'editor'
+    end
 
-Factory.define :manager, :parent => :user do |f|
-  f.role 'manager'
-end
+    factory :executive do
+      role 'executive'
+    end
 
-Factory.define :editor, :parent => :user do |f|
-  f.role 'editor'
-end
+    factory :manager do
+      role 'manager'
+    end
 
-Factory.define :executive, :parent => :user do |f|
-  f.role 'executive'
-end
+    factory :sysadmin do
+      admin true
+    end
+  
+    factory :disabled_user do
+      disabled true
+    end
 
-Factory.define :new_user, :parent => :user do |f|
-  f.organization Factory(:new_organization)
-end
+    factory :new_user do
+      association :organization, :factory => :new_organization
+    end
 
-Factory.define :disabled_user, :parent => :user do |f|
-  f.disabled true
-end
-
-Factory.define :crisis_user, :parent => :user do |f|
-  f.organization Factory(:crisis_organization)
-end
-
-Factory.define :sysadmin, :parent => :user do |f|
-  f.admin true
+    factory :crisis_user do
+      association :organization, :factory => :crisis_organization
+    end
+  
+  end
+ 
 end
