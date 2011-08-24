0493a2b69111ced708829061e4f49ae57fa1fdb7
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-08-24 13:15:43 -0400

    dynamic assignment of bucket from env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@
       :aws_secret_access_key  => 'NwrfqOX7j3ahWG8FQhzKhra4c+eOsSMP6N2v14un',
       :region                 => 'us-east-1'
     }
-    config.fog_directory  = 'fracturedatlas-artsready-dev'
+    config.fog_directory  = "fracturedatlas-artsready-#{Rails.env}"
     # config.fog_host       = 'http://artsready-staging.s3-website-us-east-1.amazonaws.com/'
     # config.fog_public     = true
     config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
