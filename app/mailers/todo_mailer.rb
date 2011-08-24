e1cf8401ab217fda5b9edbd0896843ee9a31280e
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-08-24 11:50:16 -0400

    Added email template for reminder

--- app/mailers/todo_mailer.rb
+++ app/mailers/todo_mailer.rb
@@ -18,4 +18,12 @@ class TodoMailer < ActionMailer::Base
          :subject => "You have a new To Do item at ArtsReady!", :bcc => 'john.paul.ashenfelter@gmail.com')
   end
   
+  def reminder(user,todo)
+    @user = user
+    @url = todo_url(todo)
+    @todo = todo
+    mail(:to => user.email,
+         :subject => "Your ArtsReady To-Do is Due!", :bcc => 'john.paul.ashenfelter@gmail.com')
+  end
+  
 end
