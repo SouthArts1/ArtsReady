25c2960ce37950ecd1d093f2ee7507a1caf0273d
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-08-25 10:57:56 -0400

    removed constant from Assessment that is in domain, fixed forms that still pointed to it

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -10,18 +10,6 @@ class Assessment < ActiveRecord::Base
 
   after_create :populate_empty_answers
 
-  CRITICAL_FUNCTIONS = [
-    {:name => 'people', :title => 'People Resources', :optional => false},
-    {:name => 'finance', :title => 'Finances & Insurance', :optional => false},
-    {:name => 'tech', :title => 'Technology', :optional => false},
-    {:name => 'performances', :title => 'Productions', :optional => 'We put on performances'},
-    {:name => 'tickets', :title => 'Tickets & Messaging', :optional => 'We manage ticket sales'},
-    {:name => 'facilities', :title => 'Facilities', :optional => 'We have our own space/facility'},
-    {:name => 'programs', :title => 'Programs', :optional => 'We manage ongoing public programs'},
-    {:name => 'grants', :title => 'Grantmaking', :optional => 'We provide grants'},
-    {:name => 'exhibits', :title => 'Exhibits', :optional => 'We put on exhibits'}
-  ]
-
   def is_complete?
     complete
   end
