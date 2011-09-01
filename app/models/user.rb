a48e34d80489cd5cddf7553ec6507e70ae37b854
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-09-01 14:13:36 -0400

    removed required title for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
   has_many :todos
   has_many :todo_notes
 
-  validates_presence_of :title
   validates_presence_of :first_name
   validates_presence_of :last_name
   validates_presence_of :email
