1562de6fc585a3f6ff4f41c4e669ca749bdadf3d
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2011-09-02 11:40:46 -0400

    so much trouble with automatically running the data part of migration -- skip and do by hand. AGAIN. Heroku makes this a pain

--- db/migrate/20110902141601_add_user_counter_cache_for_orgs.rb
+++ db/migrate/20110902141601_add_user_counter_cache_for_orgs.rb
@@ -1,6 +1,6 @@
 class AddUserCounterCacheForOrgs < ActiveRecord::Migration
   def self.up
-    add_column :organizations, :users_count, :integer, :default => 0
+    # add_column :organizations, :users_count, :integer, :default => 0
     
     # Organization.reset_column_information
     # Organization.all.each {|p| p.update_counters p.id, :users_count => p.users.length}
