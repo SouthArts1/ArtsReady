1169e4e14b64238714c57e384d57a978248fb3ca
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2012-01-25 12:34:52 -0500

    updated debug tools

--- config/initializers/development_tweaks.rb
+++ config/initializers/development_tweaks.rb
@@ -3,26 +3,6 @@ if Rails.env.development?
   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if defined?(DevelopmentMailInterceptor)
 
   Footnotes.run! if defined?(Footnotes)
-
-  if defined?(ActiveReload)
-
-    puts Rails.logger.debug("Registering notifications for ActiveReload gem")
-
-    ActiveSupport::Notifications.subscribe("active_reload.set_clear_dependencies_hook_replaced") do |*args|
-      event = ActiveSupport::Notifications::Event.new(*args)
-      msg = event.name
-      # Ubuntu: https://github.com/splattael/libnotify, Example: Libnotify.show(:body => msg, :summary => Rails.application.class.name, :timeout => 2.5, :append => true)
-      # Macos: http://segment7.net/projects/ruby/growl/
-      puts Rails.logger.warn(" --- #{msg} --- ")
-    end
-
-    ActiveSupport::Notifications.subscribe("active_support.dependencies.clear") do |*args|
-      msg = "Code reloaded!"
-      # Ubuntu: https://github.com/splattael/libnotify, Example: Libnotify.show(:body => msg, :summary => Rails.application.class.name, :timeout => 2.5, :append => true)
-      # Macos: http://segment7.net/projects/ruby/growl/
-      puts Rails.logger.info(" --- #{msg} --- ")
-    end  
-
-  end
+  Footnotes::Notes::AssignsNote.ignored_assigns += [:@_env]
 
 end
--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor  
   def self.delivering_email(message)  
     message.subject = "[#{message.to}] #{message.subject}"  
-    message.to = "johnpaulashenfelter@gmail.com"  
+    message.to = "user@local.host"  
   end  
 end
