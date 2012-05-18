6c5829f06268bea7c8f05503c0ffe9dae5dfe50b
Kevin Hopkins <kevin@wearefound.com>
2012-05-18 13:30:43 -0400

    schema change after migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120516140115) do
+ActiveRecord::Schema.define(:version => 20120516195606) do
 
   create_table "action_items", :force => true do |t|
     t.string   "description"
@@ -195,6 +195,13 @@ ActiveRecord::Schema.define(:version => 20120516140115) do
     t.string   "title"
     t.string   "slug"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "payment_variables", :force => true do |t|
+    t.string   "key"
+    t.string   "value"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
