b1e6387fc9b71b3fcb90c3980825a977c23d6f36
K. Stewart <ignitiant@gmail.com>
2012-05-28 15:13:59 -0500

    extracted settings navbar into a partial

--- app/controllers/archived_assessments_controller.rb
+++ app/controllers/archived_assessments_controller.rb
@@ -1,3 +1,7 @@
 class ArchivedAssessmentsController < ApplicationController
 
+  def index
+
+  end
+
 end
--- spec/views/shared/_settings_navbar.html_spec.rb
+++ spec/views/shared/_settings_navbar.html_spec.rb
@@ -5,11 +5,14 @@ describe "shared/_settings_navbar" do
   
   before do
     view.stubs(:current_org).returns(current_org)
+    view.stubs(:current_settings_tab).returns(:our_team)
   end
 
   context 'given archived assessments' do
     before do
       2.times { Factory.create(:assessment, :organization => current_org) }
+      
+      render
     end
     
     it 'links to them' do
