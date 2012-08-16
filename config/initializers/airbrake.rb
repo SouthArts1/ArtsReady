6f2c3aa0a06b2c17c12c71158e7bad36c4c56f8d
Erik Ostrom <erik@echographia.com>
2012-08-15 19:02:20 -0500

    Configured new Airbrake key.
    Also set environment_name so that (I hope) we can distinguish production
    from staging in error reports.
    
    https://www.pivotaltracker.com/story/show/30478819

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = '6c02accc07cf9564f1ad3036b5400666'
+  config.api_key = '138a1df23e26f147770ad51746a4a67d'
+  config.environment_name = `hostname`
 end
+
