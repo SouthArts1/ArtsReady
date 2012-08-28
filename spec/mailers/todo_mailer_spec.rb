40d45000bd5048a0633a0b388736ab3ff0c71648
Erik Ostrom <erik@echographia.com>
2012-08-28 17:18:31 -0500

    Don't crash sending reminders for unassigned todos.
    https://www.pivotaltracker.com/story/show/29551461

--- spec/mailers/todo_mailer_spec.rb
+++ spec/mailers/todo_mailer_spec.rb
@@ -1,5 +1,18 @@
 require "spec_helper"
 
 describe TodoMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#reminder' do
+    let(:user) { FactoryGirl.create(:user, :email => 'todo@example.com') }
+    let(:organization) { user.organization }
+    let(:todo) { FactoryGirl.build_stubbed(:todo, :organization => organization) }
+
+    it 'gets a recipient list from the todo' do
+      todo.expects(:reminder_recipients).returns([user])
+      TodoMailer.expects(:mail).with do |options|
+        options[:to] == 'todo@example.com'
+      end
+
+      TodoMailer.reminder(todo)
+    end
+  end
 end
