68137cf796090da5607ed5e7c84026dda76de18e
K. Stewart <ignitiant@gmail.com>
2013-01-03 18:50:23 -0600

    Fix broken link in BB dissolution mailer
    with tests to prevent us ever again suffering this shame.
    https://www.pivotaltracker.com/story/show/34583559

--- spec/mailers/organization_mailer_spec.rb
+++ spec/mailers/organization_mailer_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe OrganizationMailer do
   describe "sign_up" do
     let(:organization) { Factory.create(:new_organization, :users => [Factory.create(:new_user)]) }
-    let(:mail) { OrganizationMailer.sign_up(organization) }
+    subject(:mail) { OrganizationMailer.sign_up(organization) }
 
     it "renders the headers" do
       mail.subject.should eq("Your ArtsReady Profile is Pending Approval")
@@ -11,13 +11,12 @@ describe OrganizationMailer do
       mail.from.should eq(["admin@artsready.org"])
     end
 
-    it "renders the body" do
-    end
+    it_behaves_like 'a mailer view'
   end
 
   describe "approved" do
     let(:organization) { Factory.create(:organization, :users => [Factory.create(:user)]) }
-    let(:mail) { OrganizationMailer.approved(organization) }
+    subject(:mail) { OrganizationMailer.approved(organization) }
 
     it "renders the headers" do
       mail.subject.should eq("#{organization.name} is now part of ArtsReady!")
@@ -25,8 +24,40 @@ describe OrganizationMailer do
       mail.from.should eq(["admin@artsready.org"])
     end
 
-    it "renders the body" do
-    end
+    it_behaves_like 'a mailer view'
   end
 
+  describe "battle buddy dissolution" do
+    let(:target_organization) {
+      Factory.create(:organization,
+                     :users => [Factory.create(:user)])
+    }
+    let(:requesting_organization) {
+      Factory.create(:organization)
+    }
+    let(:user) { target_organization.users.first }
+    subject(:mail) {
+      OrganizationMailer.battle_buddy_dissolution(
+        user, target_organization, requesting_organization)
+    }
+
+    it_behaves_like 'a mailer view'
+  end
+
+  describe "battle buddy invitation" do
+    let(:target_organization) {
+      Factory.create(:organization,
+                     :users => [Factory.create(:user)])
+    }
+    let(:requesting_organization) {
+      Factory.create(:organization)
+    }
+    let(:user) { target_organization.users.first }
+    subject(:mail) {
+      OrganizationMailer.battle_buddy_invitation(
+        user, target_organization, requesting_organization)
+    }
+
+    it_behaves_like 'a mailer view'
+  end
 end
