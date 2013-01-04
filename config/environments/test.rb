7cae8a76499d25a50f065838f76ad1612a44f2e1
K. Stewart <ignitiant@gmail.com>
2013-01-03 18:49:05 -0600

    Use valid hostname in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,7 @@ Artsready::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   
-  config.action_mailer.default_url_options = { :host => 'artsready_test.local' }
+  config.action_mailer.default_url_options = { :host => 'artsreadytest.local' }
 end
 
 MAILCHIMP_API_KEY = 'not-required'
