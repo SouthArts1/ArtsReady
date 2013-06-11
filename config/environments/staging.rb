0c06a75ba52b6e0d21654de93de166b517fe8a62
Erik Ostrom <erik@echographia.com>
2013-06-11 16:36:12 -0500

    Don't use SendGrid in test and dev environments.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -52,7 +52,17 @@ Artsready::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-    
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   config.middleware.insert_before ActionDispatch::Static, "Rack::SSL"
 end
 
