fbd7bea88915a8898e390fa5fc15f6ddc03574e6
Erik Ostrom <erik@echographia.com>
2014-03-26 13:16:28 -0500

    Rewrote request specs to be request specs.
    They were integration tests in disguise, using Capybara, which is
    no longer supported in request specs as of Capybara 2.0.

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -3,49 +3,63 @@ require 'spec_helper'
 describe "PasswordResets" do
   it "emails user when requesting password reset" do
     user = Factory.create(:user)
-    visit sign_in_path
-    click_link "password"
-    fill_in "Email", :with => user.email
-    click_button "Reset Password"
-    current_path.should eq(root_path)
-    page.should have_content("Email sent")
-    last_email.to.should include(user.email)
+    reset_email
+
+    post password_resets_path, email: user.email
+    expect(response).to redirect_to root_path
+
+    follow_redirect!
+    expect(response.body).to include 'Email sent'
+    expect(last_email.to).to include user.email
   end
 
   it "does not email invalid user when requesting password reset" do
-    visit sign_in_path
-    click_link "password"
-    fill_in "Email", :with => "nobody@example.com"
-    click_button "Reset Password"
-    current_path.should eq(root_path)
-    page.should have_content("Email sent")
-    last_email.should be_nil
+    post password_resets_path, email: 'nobody@example.com'
+    expect(response).to redirect_to root_path
+
+    follow_redirect!
+    expect(response.body). to include 'Email sent'
+    expect(last_email).to be_nil
   end
 
   it "updates the user password when confirmation matches" do
     user = Factory.create(:user, :password_reset_token => "something", :password_reset_sent_at => 1.hour.ago)
-    visit edit_password_reset_path(user.password_reset_token)
-    fill_in "Password", :with => "foobar"
-    click_button "Update Password"
-    page.should have_content("Password doesn't match confirmation")
-    fill_in "Password", :with => "foobar"
-    fill_in "Password confirmation", :with => "foobar"
-    click_button "Update Password"
-    page.should have_content("Password has been reset")
+
+    put password_reset_path(user.password_reset_token,
+      user: {
+        password: 'foobar',
+        password_confirmation: ''
+      }
+    )
+    expect(response.body).to include("match confirmation")
+
+    put password_reset_path(user.password_reset_token,
+      user: {
+        password: 'foobar',
+        password_confirmation: 'foobar'
+      }
+    )
+    expect(response).to redirect_to root_path
+    follow_redirect!
+    expect(response.body).to include("Password has been reset")
   end
 
   it "reports when password token has expired" do
     user = Factory.create(:user, :password_reset_token => "something", :password_reset_sent_at => 5.days.ago)
-    visit edit_password_reset_path(user.password_reset_token)
-    fill_in "Password", :with => "foobar"
-    fill_in "Password confirmation", :with => "foobar"
-    click_button "Update Password"
-    page.should have_content("Password reset has expired")
+    put password_reset_path(user.password_reset_token,
+      user: {
+        password: 'foobar',
+        password_confirmation: ''
+      }
+    )
+    expect(response).to redirect_to new_password_reset_path
+    follow_redirect!
+    expect(response.body).to include("Password reset has expired")
   end
 
   it "raises record not found when password token is invalid" do
     lambda {
-      visit edit_password_reset_path("invalid")
+      get edit_password_reset_path("invalid")
     }.should raise_exception(ActiveRecord::RecordNotFound)
   end
 end
