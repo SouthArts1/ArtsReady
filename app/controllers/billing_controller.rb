723d623da2456955aa8c230b44a0f64f7de20852
Erik Ostrom <erik@echographia.com>
2014-03-26 11:58:09 -0500

    Refactor: unduplicated code in billings#create/update.
    Most of this code should be implemented by the model, and/or just by
    setting @payment.attributes = params[:payment]. But this is a safe
    improvement for now.

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -1,5 +1,9 @@
 class BillingController < ApplicationController
   skip_before_filter :authenticate!, only: [:new, :create, :get_discount]
+  UNSPECIFIED_ERROR_MESSAGE =
+    "There was a problem processing your request. " +
+    "Please check your billing address and payment information and try again."
+
   def new
     if current_org || (session[:organization_id] != nil && session[:organization_id] != "")
       @page = Page.find_by_slug('billing') #rescue OpenStruct(:title => 'Billing', :body => 'Body', :slug => 'Billing')
@@ -50,62 +54,18 @@ class BillingController < ApplicationController
   end
   
   def create
-    obj = params[:payment]
-    @organization = Organization.find(obj[:organization_id])
+    @organization = current_org
+    @payment = @organization.payments.build
+    set_payment_attributes
 
-    start_amount = PaymentVariable.find_by_key("starting_amount_in_cents").value.to_f
-    regular_amount = PaymentVariable.find_by_key("regular_amount_in_cents").value.to_f
-
-    @payment = Payment.new({
-      organization_id: obj[:organization_id],
-      billing_first_name: obj[:billing_first_name],
-      billing_last_name: obj[:billing_last_name],
-      billing_address: obj[:billing_address],
-      billing_city: obj[:billing_city],
-      billing_state: obj[:billing_state],
-      billing_zipcode: obj[:billing_zipcode],
-      billing_email: obj[:billing_email],
-      payment_type: params[:payment_type],
-      starting_amount_in_cents: start_amount,
-      regular_amount_in_cents: regular_amount
-    })
-
-    if session[:discount_code]
-      begin
-        d = DiscountCode.find(obj[:discount_code_id])
-        @payment.discount_code_id = d.id
-        @payment.validate_discount_code!
-      rescue Exception => e
-        # do nothing
-      end
-    end
-    if params[:payment_type] == "cc"
-      @payment.payment_type = "cc"
-      @payment.number = obj[:number]
-      @payment.ccv = obj[:ccv]
-      @payment.expiry_month = obj[:expiry_month]
-      @payment.expiry_year = obj[:expiry_year]
-    elsif params[:payment_type] == "bank"
-      @payment.payment_type = "bank"
-      @payment.account_type = obj[:account_type].downcase
-      @payment.bank_name = obj[:bank_name]
-      @payment.routing_number = obj[:routing_number]
-      @payment.account_number = obj[:account_number]
-    else
-      puts("PAYMENT TYPE #{params[:payment_type].inspect}")
-      return redirect_to :back, notice: "There was a problem processing your request.  Please check your billing address and payment information and try again."
-    end
     if @payment.save
       session[:discount_code] = nil
-      @current_user = @organization.managers.first
-      session[:user_id] = @current_user.id
-      return redirect_to "/" 
+      redirect_to "/"
     else
-      puts("PAYMENT ERRORS: #{@payment.errors.inspect}")
-      return redirect_to :back, notice: "There was a problem processing your request.  Please check your billing address and payment information and try again."
+      redirect_to :back, notice: UNSPECIFIED_ERROR_MESSAGE
     end
   end
-  
+
   def edit
     @organization = current_org
     @payment = @organization.payment
@@ -121,54 +81,17 @@ class BillingController < ApplicationController
   end
   
   def update
-    obj = params[:payment]
     @organization = current_org
-    
-    start_amount = PaymentVariable.find_by_key("starting_amount_in_cents").value.to_f
-    regular_amount = PaymentVariable.find_by_key("regular_amount_in_cents").value.to_f
-    
-    @payment = current_org.payment
-    redirect_to :back, notice: "There was a problem processing your request.  Please check your billing address and payment information and try again." unless @payment
-    
-    if session[:discount_code]
-      begin
-        d = DiscountCode.find_by_discount_code(session[:discount_code])
-        @payment.discount_code_id = d.id
-        @payment.validate_discount_code!
-      rescue Exception => e
-        # do nothing
-      end
-    end
-    
-    if params[:payment_type] == "cc"
-      @payment.number = obj[:number]
-      @payment.ccv = obj[:ccv]
-      @payment.payment_type = "cc"
-      @payment.expiry_month = obj[:expiry_month]
-      @payment.expiry_year = obj[:expiry_year]
-    elsif params[:payment_type] == "bank"
-      @payment.account_type = obj[:account_type].downcase
-      @payment.bank_name = obj[:bank_name]
-      @payment.routing_number = obj[:routing_number]
-      @payment.account_number = obj[:account_number]
-      @payment.payment_type = "bank"
-    else
-      return redirect_to :back, notice: "There was a problem processing your request.  Please check your billing address and payment information and try again."
-    end
-    
-    @payment.billing_first_name = obj[:billing_first_name] 
-    @payment.billing_last_name = obj[:billing_last_name] 
-    @payment.billing_address = obj[:billing_address] 
-    @payment.billing_city = obj[:billing_city] 
-    @payment.billing_state = obj[:billing_state] 
-    @payment.billing_zipcode = obj[:billing_zipcode]  
-    @payment.billing_email = obj[:billing_email]  
+    @payment = @organization.payment
+    set_payment_attributes
+
+    return redirect_to(:back, notice: UNSPECIFIED_ERROR_MESSAGE) if !@payment
 
     if @payment.save
       session[:discount_code] = nil
       redirect_to billing_path
     else
-      redirect_to :back, notice: "There was a problem processing your request.  Please check your billing address and payment information and try again."
+      redirect_to :back, notice: UNSPECIFIED_ERROR_MESSAGE
     end
   end
   
@@ -194,6 +117,59 @@ class BillingController < ApplicationController
       end
     else 
       redirect_to :back, notice: "There was a problem cancelling your subscription.  Please contact ArtsReady for assistance."
+    end
+  end
+
+  private
+
+  def set_payment_attributes
+    return if !@payment
+
+    payment_params = params[:payment]
+
+    @payment.attributes = {
+      billing_first_name: payment_params[:billing_first_name],
+      billing_last_name:  payment_params[:billing_last_name],
+      billing_address:    payment_params[:billing_address],
+      billing_city:       payment_params[:billing_city],
+      billing_state:      payment_params[:billing_state],
+      billing_zipcode:    payment_params[:billing_zipcode],
+      billing_email:      payment_params[:billing_email]
+    }
+
+    if @payment.new_record?
+      @payment.attributes = {
+        starting_amount_in_cents:
+          PaymentVariable.find_by_key("starting_amount_in_cents").value.to_f,
+        regular_amount_in_cents:
+          PaymentVariable.find_by_key("regular_amount_in_cents").value.to_f
+      }
+    end
+
+    if session[:discount_code]
+      begin
+        d = DiscountCode.find(payment_params[:discount_code_id])
+        @payment.discount_code_id = d.id
+        @payment.validate_discount_code!
+      rescue Exception => e
+        # do nothing
+      end
+    end
+
+    @payment.payment_type = params[:payment_type]
+
+    if @payment.payment_type == "cc"
+      @payment.number       = payment_params[:number]
+      @payment.ccv          = payment_params[:ccv]
+      @payment.expiry_month = payment_params[:expiry_month]
+      @payment.expiry_year  = payment_params[:expiry_year]
+    elsif @payment.payment_type == "bank"
+      @payment.account_type   = payment_params[:account_type].downcase
+      @payment.bank_name      = payment_params[:bank_name]
+      @payment.routing_number = payment_params[:routing_number]
+      @payment.account_number = payment_params[:account_number]
+    else
+      # payment type is invalid, let `save` take care of it
     end
   end
 end
