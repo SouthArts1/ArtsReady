1dfb49c8c8ea3a6d3e41348343eaa69fbdba4e2c
Erik Ostrom <erik@echographia.com>
2014-04-17 13:07:23 -0500

    Refactor: Prettied up some PaymentNotification code.

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -5,15 +5,15 @@ class PaymentNotification < ActiveRecord::Base
   serialize :params
 
   def capture?
-    param(:x_type) =~ /CAPTURE/i
+    type =~ /CAPTURE/i
   end
 
   def success?
-    param(:x_response_code) == '1'
+    response_code == '1'
   end
 
   def subscription
-    Subscription.find_by_arb_id(param(:x_subscription_id))
+    Subscription.find_by_arb_id(subscription_id)
   end
 
   # Handle "notification.foo" by returning "params['x_foo']" if available.
